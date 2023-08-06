5d5deaa959cbea4be41b7bd8fa914453c0f1630e
Josh Blood <jblood92@pm.me>
2023-08-06 13:58:45 -0400

    fetch and display todos, form started

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from "react";
 
-const URL = 'http://localhost:9000/api/todos'
+const URL = "http://localhost:9000/api/todos";
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch(URL)
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json.data);
+        this.setState({
+          todos: json.data,
+        });
+      });
+  }
+
   render() {
-    return null
+    return (
+      <div className="todo">
+        <h2>ToDos:</h2>
+        {this.state.todos.map((todo) => (
+          <div key={todo.id}>
+            <p>
+              <span>{todo.completed ? "\u2612 " : "\u2610 "}- </span>
+              {todo.name}
+            </p>
+          </div>
+        ))}
+        <hr />
+        <form>
+          <input type="text" placeholder="Add a new todo" />
+          <button>Add</button>
+        </form>
+        <hr />
+        <button className="clear-btn">Hide completed</button>
+      </div>
+    );
   }
 }
