14c6c7851df898b0dbffa7996180f281ce31f70b
Josh Blood <jblood92@pm.me>
2023-08-06 20:21:43 -0400

    axios fetch and catch

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import React from "react";
 
 const URL = "http://localhost:9000/api/todos";
@@ -11,28 +12,41 @@ export default class App extends React.Component {
   }
 
   componentDidMount() {
-    fetch(URL)
-      .then((response) => response.json())
-      .then((json) => {
-        console.log(json.data);
+    // fetch(URL)
+    //   .then((response) => response.json())
+    //   .then((json) => {
+    //     console.log(json.data);
+    //     this.setState({
+    //       todos: json.data,
+    //     });
+    //   });
+    axios
+      .get(URL)
+      .then((response) => {
+        console.log(response.data.data);
         this.setState({
-          todos: json.data,
+          todos: response.data.data,
         });
+      })
+      .catch((error) => {
+        console.log(error);
       });
   }
 
   render() {
     return (
       <div className="todo">
-        <h2>ToDos:</h2>
-        {this.state.todos.map((todo) => (
-          <div key={todo.id}>
-            <p>
-              <span>{todo.completed ? "\u2612 " : "\u2610 "}- </span>
-              {todo.name}
-            </p>
-          </div>
-        ))}
+        <div id="todos">
+          <h2>ToDos:</h2>
+          {this.state.todos.map((todo) => (
+            <div key={todo.id}>
+              <p>
+                <span>{todo.completed ? "\u2612 " : "\u2610 "}- </span>
+                {todo.name}
+              </p>
+            </div>
+          ))}
+        </div>
         <hr />
         <form>
           <input type="text" placeholder="Add a new todo" />
